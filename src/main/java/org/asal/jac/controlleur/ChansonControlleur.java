e4f93964018405e822b6619bdf5af6a045cbc65c
asettouf <adescarte@etu.emse.fr>
2014-02-23 22:08:49 +0100

    Adding song operationnal

--- src/main/java/org/asal/jac/controlleur/ChansonControlleur.java
+++ src/main/java/org/asal/jac/controlleur/ChansonControlleur.java
@@ -6,7 +6,9 @@ import org.asal.jac.domain.Chanson;
 import org.asal.jac.service.ChansonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,6 +27,18 @@ public class ChansonControlleur {
 		Collection<Chanson> cList=cService.findAllChanson();
 		map.addAttribute("cList",cList);
 		return "chanson";
+	}
+	
+	@RequestMapping(value="/cChanson", method=RequestMethod.GET)
+	public String returnChanson(Model mod){
+		mod.addAttribute("chanson", new Chanson());
+		return "createChanson";
+	}
+
+	@RequestMapping(value="/addChanson", method=RequestMethod.POST)
+	public String returnChanson(@ModelAttribute("chanson")Chanson ch){
+		cService.createChanson(ch);
+		return "createChanson";
 	}
 
 }
